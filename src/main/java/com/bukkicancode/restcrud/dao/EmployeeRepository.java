f06c21f2168e613077c0189c6207d20d5af17bac
pbukki <philipbukkie@gmail.com>
2023-07-02 12:52:03 +0300

    implemented spring data rest and configs

--- src/main/java/com/bukkicancode/restcrud/dao/EmployeeRepository.java
+++ src/main/java/com/bukkicancode/restcrud/dao/EmployeeRepository.java
@@ -2,6 +2,9 @@ package com.bukkicancode.restcrud.dao;
 
 import com.bukkicancode.restcrud.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+//Annotation to specify different path name ie members
+//@RepositoryRestResource(path="members")
 public interface EmployeeRepository extends JpaRepository<Employee,Integer> {
 }
